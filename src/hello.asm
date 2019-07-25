7ba1847a77bcf302035dc787344df104d9b37823
rom1 <rom1@canel.ch>
2019-07-25 19:33:59 +0200

    Premier programme HELLO + Makefile

---
+++ src/hello.asm
@@ -0,0 +1,21 @@
+!source "./libs/bootloader.asm"
++start_at $1000
+
+
+lda #8		;set2 H
+sta $400	;H in 400 screen memory
+
+lda #5		;set2 E
+sta $401	;E in 400+1 screen memory
+
+lda #12		;set2 L
+sta $402	;E in 400+2 screen memory
+
+lda #12		;set2 L
+sta $403	;E in 400+3 screen memory
+
+lda #15		;set2 O
+sta $404	;E in 400+4 screen memory
+
+end:
+        jmp end
